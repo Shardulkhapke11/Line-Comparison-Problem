f748ab0d010e44309ea90fb39f04c2bd77508e0c
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-16 03:13:37 +0530

    Added: UC1

--- Employee.java
+++ Employee.java
@@ -1,5 +1,15 @@
-public class Employee {
-public static void main(String[] args){
-System.out.println("Welcome to Line Comparison Problem");
-}
+	public class Employee {
+
+	public static void main(String[] args) {
+int x1=4;
+int y1=3;
+int x2=8;
+int y2=6;
+System.out.println("the cordinates of line are");
+System.out.println("(" + x1 + "," + y1 + ")");
+System.out.println("(" + x2 + "," + y2 + ")");
+double length=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))); 
+		System.out.println(length);
+		
+	}
 }
