6e74447cc6735cca8f21962c602fe83671fbb3e6
Shardulkhapke11 <shardulkhapke04@gmail.com>
2021-06-16 03:44:53 +0530

    Added: UC3

--- Employee.java
+++ Employee.java
@@ -1,4 +1,4 @@
-public class cartesian {
+public class Employee {
 
 	public static void main(String[] args) {
 int x1=1;
@@ -16,14 +16,22 @@ double length=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
 		System.out.println(length);
 		String str = Double.toString(length);
 System.out.println("the cordinates of line 2 are");
-System.out.println("(" + x3 + "," + y3 + ")");
+		System.out.println("(" + x3 + "," + y3 + ")");
 		System.out.println("(" + x4 + "," + y4 + ")");
 		double length2=Math.sqrt(((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3))); 
 				System.out.println(length2);
 				String str2 = Double.toString(length2);
-				 if (str.equals(str2))
-			            System.out.println("Equal");
-			        else
-			            System.out.println("Not equal");
+				System.out.println(str2.compareTo(str));
+				// if (str.equals(str2))
+			      //      System.out.println("equal");
+			        //else
+			          //  System.out.println("not equal");
+				 if(str2.compareTo(str)>0)
+					 System.out.println( "Length2 is greater than length1" );
+				 else if(str2.compareTo(str)==0)
+					 System.out.println( "length2 is equal to length1");
+				 else 
+					 System.out.println("length2 is smaller than length");
+					 
 	}
 }
